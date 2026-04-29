d78083f7cd536bd7342c8f30a6dfc97c48b3dbd2
ERICKQUISPEQUS <163932065+ERICKQUISPEQUS@users.noreply.github.com>
2026-04-29 16:28:00 -0500

    Update pimerJS.js

---
+++ js/pimerJS.js
@@ -0,0 +1,60 @@
+// Console.log nos sirve para depurar y probar nuestras salidas
+console.log("Hola mundo desde JavaScript");
+//alert nos sirve para mostrarle un alert al usuario
+alert("Hola Mundo. Esto es una Alerta");
+
+function cambiarCarita(){
+    document.getElementById("carita").src="imagenes/triste.jpg";
+    document.getElementById("logoSenati").src="imagenes/senati1.png";
+    alert("Por que tas triste");
+}
+function feliz(){
+    document.getElementById("carita").src="imagenes/feliz.jpg";
+    document.getElementById("TituloJS").textContent="Ejemplo de Caritas";
+}
+function nombresNav(){
+    document.getElementById("m1").textContent="index";
+    document.getElementById("m2").textContent="segunda";
+    document.getElementById("m3").textContent="tercera";
+    document.getElementById("m4").textContent="cuarta";
+    document.getElementById("m5").textContent="quinta";
+    document.getElementById("m6").textContent="Sexta Página";
+}
+
+function cambiarTitulo(){
+    document.getElementById("titulo").textContent="Practicando JS";
+    document.getElementById("titulo").style.color="#290c60" 
+    document.getElementById("logoSenati").src="imagenes/senati3.jpg";
+    document.getElementById("encabezado").style.background="#89cce2"
+}
+
+function suma(){
+    let num1 = 3;
+    let num2 = 5;
+    let suma = num1+num2;
+    alert("la suma es de" +suma);
+} 
+function eje01(){
+    let p1 = 10;
+    let p2 = 5;
+    let p3 = 15;
+    let c1 = 3;
+    let c2 = 18;
+    let c3 = 6;
+    let Stotal, total;
+    Stotal = (p1*c1)+(p2*c2)+(p3*c3);
+    if(Stotal>100){
+        total = Stotal*0.95;
+        alert("Su compra lleva descuento");
+    }else{
+        total = Stotal;
+    }
+    alert("El total a Pagar es de: S/."+total);
+
+}
+function eje02(){
+    let amigos = 10;
+    let total = 100;
+    let cuota = total/amigos;
+    alert("Son "+amigos+" amigos"+ " y la cuota del total S/."+total+" es de : S/."+cuota);
+}
